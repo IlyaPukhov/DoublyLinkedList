d9a7078ce04ceb830402422d4b25ce9b7088851b
Ilya Pukhov <IlyaPukhov@github.com>
2021-11-25 23:15:49 +0300

    DoublyLinkedList

--- src/main/kotlin/DoublyLinkedList.kt
+++ src/main/kotlin/DoublyLinkedList.kt
@@ -1,27 +1,45 @@
 class DoublyLinkedList<T> {
     var head: Node<T>? = null
-    var tail: Node<T>? = null
+    private var tail: Node<T>? = null
 
+    override fun toString() = head?.toString() ?: "empty list"
 
-    /** Добавление элемента в голову двусвязного списка. */
-    fun push(x: T) {
-        val node = Node(x)
-        if (head == null) {   //если список пуст, то голова и хвост - это один узел
-            head = node
-            tail = node
-        } else {
-            val oldHead = head //сохраняем "старую" голову
-            head = Node(x, oldHead, null) //создаем "новую" голову со ссылками на прошлую и на null
-            oldHead?.prev = head
-        }
+    private val isEmpty: Boolean get() {
+        return head == null
     }
 
-    /** Добавление элемента в хвост двусвязного списка. */
-    fun addTail(x: T) {
+    fun push(x: T) {
         val node = Node(x)
-        if (head == null) {    //если список пуст, то голова и хвост - это один узел
+        if (isEmpty) {
             head = node
             tail = node
+            return
+        }
+        node.next = head
+        head!!.prev = node
+        head = node
+    }
+
+    fun pop(): T? {
+        if (isEmpty) {
+            return null
+        }
+        val value = head?.value
+        head = head?.next
+        if (head == null) {
+            tail = null
+        } else {
+            head!!.prev = null
+        }
+        return value
+    }
+
+    fun addTail(x: T) {
+        val node = Node(x)
+        if (isEmpty) {    //если список пуст, то голова и хвост - это один узел
+            head = node
+            tail = node
+            return
         } else {
             val oldTail = tail //сохраняем "старый" хвост
             tail = Node(x, null, oldTail) //создаем "новый" хвост со ссылками на прошлую и на null
@@ -29,19 +47,50 @@ class DoublyLinkedList<T> {
         }
     }
 
-    /** Удаление элемента из головы двусвязного списка. */
-    fun pop(): T? {
-        val value = head?.value //сохраняем значение "старой" головы
-        head = head?.next //голова теперь - узел, находившийся за "старой" головой
-        head?.prev = null //новая голова ссылается на null
-        return value //возвращаем значение удаленной головы
-    }
 
-    /** Удаление элемента из хвоста двусвязного списка. */
     fun delTail(): T? {
+        if (isEmpty) {
+            return null
+        }
         val value = tail?.value //сохраняем значение "старого" хвоста
         tail = tail?.prev //хвост теперь - узел, находившийся перед "старым" хвостом
         tail?.next = null //новый хвост ссылается на null
         return value //возвращаем значение удаленного хвоста
+    }
+
+    private fun findByIndex(index: Int): Node<T>? {
+        var node = head
+        var i = 0
+        while (node != null && i < index) {
+            node = node.next
+            i++
+        }
+        return node
+    }
+
+    /*
+          Задача. Найти середину связного списка за один проход по св. списку.
+     */
+
+    fun findMiddle(): Node<T>? {
+        var node = head
+        var node2 = head
+        while (node != null && node2?.next != null) {
+            node = node.next
+            node2 = node2.next?.next
+        }
+        return node
+    }
+
+    fun removeAtIndex(index: Int): T? {
+        if (index == 0) {
+            return pop()
+        }
+        val a = findByIndex(index - 1)
+        val b = a?.next
+        val c = b?.next
+        a?.next = c
+        c?.prev = a
+        return b?.value
     }
 }
